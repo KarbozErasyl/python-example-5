b5de2ae6dbfa6533ee388f82e6e3b77fc1116875
Yerassyl <157903613+KarbozErasyl@users.noreply.github.com>
2026-03-02 08:09:15 +0500

    Update python 6.py

--- python 6.py
+++ python 6.py
@@ -20,3 +20,13 @@ is_citizen = True
 age = 25
 
 print(is_citizen and age) #25
+
+
+is_citizen = True
+age = 25
+
+if is_citizen and age >=18:
+	print('You are eligible to vote') #You are eligible to vote
+	
+else:
+	print('You are not eligible to vote')
